a6cb8febc6bd84fc99c4c89033bf16da6d699500
Leonardo <leonardocarlos807@gmail.com>
2025-05-10 12:12:44 -0300

    Projeto Pedido de Namoro

---
+++ script.js
@@ -0,0 +1,53 @@
+const images = document.querySelectorAll('.slideshow img');
+let currentIndex = 0;
+
+function changeImage() {
+  images[currentIndex].classList.remove('active');
+  currentIndex = (currentIndex + 1) % images.length;
+  images[currentIndex].classList.add('active');
+}
+
+setInterval(changeImage, 3000);
+
+const yesBtn = document.getElementById('yesBtn');
+const message = document.getElementById('message');
+const loveMessage = document.getElementById('loveMessage');
+
+yesBtn.addEventListener('click', () => {
+  message.style.display = 'block';
+  loveMessage.style.display = 'block';
+  setTimeout(() => {
+    loveMessage.style.display = 'none';
+  }, 12000); // A mensagem some após 12 segundos
+});
+
+const audio = document.getElementById('audio');
+const playPauseBtn = document.getElementById('playPauseBtn');
+let isPlaying = false;
+
+playPauseBtn.addEventListener('click', () => {
+  if (isPlaying) {
+    audio.pause();
+    playPauseBtn.textContent = '▶️';
+  } else {
+    audio.play();
+    playPauseBtn.textContent = '⏸️';
+  }
+  isPlaying = !isPlaying;
+});
+
+function createHeart() {
+  const heart = document.createElement('div');
+  heart.classList.add('heart');
+  heart.innerHTML = '❤️';
+  const centerRange = 40; // Concentrando os corações entre 30% e 70% da largura da tela
+  heart.style.left = (Math.random() * centerRange + (50 - centerRange / 2)) + 'vw';
+  heart.style.animationDuration = Math.random() * 3 + 2 + 's';
+  document.body.appendChild(heart);
+
+  setTimeout(() => {
+    heart.remove();
+  }, 5000);
+}
+
+setInterval(createHeart, 300);
